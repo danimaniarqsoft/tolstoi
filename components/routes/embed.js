2021cb62cc03a1d949569e31aa7ac84bbfeea5da
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-05-04 15:34:30 -0500

    adding default template

--- components/routes/embed.js
+++ components/routes/embed.js
@@ -1,12 +1,21 @@
 module.exports = function(webserver, controller) {
 
 
+// This create the /site route, where an easy-to-copy embed code is available
+webserver.get('/site', function(req,res) {
+  res.render('site', {
+    layout: 'layouts/default',
+    base_url: req.headers.host
+  });
+
+});
+
 // This creates the /embed route, where an easy-to-copy embed code is available
 webserver.get('/embed', function(req,res) {
 
     res.render('embed', {
       layout: 'layouts/default',
-      base_url: req.hostname
+      base_url: req.headers.host
     });
 
 });
@@ -16,10 +25,9 @@ webserver.get('/', function(req,res) {
 
     res.render('index', {
       layout: 'layouts/default',
-      base_url: req.hostname
+      base_url: req.headers.host
     });
 
 });
-
 
 }
