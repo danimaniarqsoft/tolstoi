122b8cf80b10c255973e85dc43ab7c2f840b12d3
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-08-18 14:48:33 -0500

    updating route

--- components/routes/embed.js
+++ components/routes/embed.js
@@ -1,45 +1,40 @@
+module.exports = function (webserver, controller) {
+  // This create the /site route, where an easy-to-copy embed code is available
+  webserver.get('/site', function (req, res) {
+    res.render('site', {
+      layout: 'layouts/default',
+      base_url: req.headers.host
+    });
 
-
-module.exports = function(webserver, controller) {
-  var env = require('node-env-file');
-  env(__dirname + '../../../.env');
-
-// This create the /site route, where an easy-to-copy embed code is available
-webserver.get('/site', function(req,res) {
-  res.render('site', {
-    layout: 'layouts/default',
-    base_url: req.headers.host
   });
 
-});
+  // This create the /site route, where an easy-to-copy embed code is available
+  webserver.get('/test', function (req, res) {
+    res.render('test', {
+      layout: 'layouts/default',
+      base_url: req.headers.host
+    });
 
-// This create the /site route, where an easy-to-copy embed code is available
-webserver.get('/test', function(req,res) {
-  res.render('test', {
-    layout: 'layouts/default',
-    base_url: req.headers.host
   });
 
-});
-
-// This creates the /embed route, where an easy-to-copy embed code is available
-webserver.get('/embed', function(req,res) {
+  // This creates the /embed route, where an easy-to-copy embed code is available
+  webserver.get('/embed', function (req, res) {
 
     res.render('embed', {
       layout: 'layouts/default',
       base_url: req.headers.host
     });
 
-});
+  });
 
-// This creates the /embed route, where an easy-to-copy embed code is available
-webserver.get('/', function(req,res) {
+  // This creates the /embed route, where an easy-to-copy embed code is available
+  webserver.get('/', function (req, res) {
 
     res.render('index', {
       layout: 'layouts/default',
       base_url: req.headers.host,
     });
 
-});
+  });
 
 }
--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -1,9 +1,9 @@
 var debug = require('debug')('botkit:incoming_webhooks');
 
-module.exports = function(webserver, controller) {
+module.exports = function (webserver, controller) {
 
     debug('Configured /botkit/receive url');
-    webserver.post('/botkit/receive', function(req, res) {
+    webserver.post('/botkit/receive', function (req, res) {
 
         // respond to Slack that the webhook has been received.
         res.status(200);
